d589b2e3e62003b0d8783ccb4657395c3f612923
Ron Darby <ron.darby@payfast.co.za>
2013-01-16 11:25:57 +0200

    Added phpDocumentor block

--- payments/payfast.php
+++ payments/payfast.php
@@ -1,5 +1,25 @@
 <?php
-
+/**
+ * payment/payfast.php
+ *
+ * Copyright (c) 2009-2012 PayFast (Pty) Ltd
+ * 
+ * LICENSE:
+ * 
+ * This payment module is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation; either version 3 of the License, or (at
+ * your option) any later version.
+ * 
+ * This payment module is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public
+ * License for more details.
+ * 
+ * @author     Ron Darby
+ * @copyright  2009-2012 PayFast (Pty) Ltd
+ * @license    http://www.opensource.org/licenses/lgpl-license.php LGPL
+ */
 if ( !defined('AREA') ) { die('Access denied'); }
 
 include('payfast/payfast_common.inc');
