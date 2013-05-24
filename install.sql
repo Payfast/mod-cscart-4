95d2f8ace57a8506e4886bcba8ac730540b5af43
Ron Darby <ron.darby@payfast.co.za>
2013-05-24 15:30:49 +0200

    Fixed Sandbox changed template names to avoid template conflicts

--- install.sql
+++ install.sql
@@ -1,4 +1,4 @@
-REPLACE INTO cscart_payment_processors (`processor_id`, `processor`,`processor_script`,`processor_template`,`admin_template`,`callback`,`type`) VALUES ('1005', 'PayFast','payfast.php', 'payfast.tpl','payfast.tpl', 'N', 'P');
+REPLACE INTO cscart_payment_processors (`processor`,`processor_script`,`processor_template`,`admin_template`,`callback`,`type`) VALUES ('PayFast','payfast.php', 'processor_payfast.tpl','admin_payfast.tpl', 'N', 'P');
 
 REPLACE INTO cscart_language_values (`lang_code`,`name`,`value`) VALUES ('EN','text_payfast_status_map','PayFast payment status to CS-Cart order status convertion map');
 
